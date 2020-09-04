9e930eaa8d1902a32df181ce02ae432104fe8fc9
Mohit <nhu7762@gmail.com>
2020-09-04 21:14:18 +0530

    My day 5 CC

---
+++ Day5/CC.py
@@ -0,0 +1,31 @@
+Q 1 : Write a program to check if the number is even or odd?
+
+
+a = int (input("Enter a number  "))
+
+if a%2==0:
+    print(a,"is an even number")
+    else:
+        print(x,"is an odd number")
+
+
+***
+
+Q 2: What will be the output of it
+
+a=2
+b=7
+((3==4)and(a<b)) or (b==7)
+
+Ans : True
+
+****
+
+Q 3: What will be the output of following
+
+A = 10
+B = 50
+
+(not(A==B)and not(B==A)) or (A!=10)
+
+Ans : True
